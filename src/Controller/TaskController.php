bd25ac681f2230512276c8cd1e0eb68e3dc4cc3d
Fred Perrier <frederique.perrier@gmail.com>
2025-06-25 13:08:10 +0200

    Add methods for task (new, edit, delete)

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Projects;
+use App\Entity\Tasks;
+use App\Form\TaskForm;
+use App\Repository\TasksRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends AbstractController
+{
+    public function __construct(private TasksRepository $tasksRepository, private EntityManagerInterface $em)
+    {
+        
+    }
+
+    #[Route('project/{projectId}/newTask', name: 'app_new_task')]
+    public function new(Request $request, int $projectId): Response
+    {
+        $project = $this->em->getRepository(Projects::class)->find($projectId);
+
+        if(!$project) {
+            throw $this->createNotFoundException('Projet introuvable');
+        }
+
+        $task = new Tasks;
+        $task->setProject($project);
+
+        $form = $this->createForm(TaskForm::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $task->setCreatedAt(new \DateTimeImmutable());
+            $task->setUpdatedAt(new \DateTimeImmutable());
+            //$task = $form->getData();
+
+            $this->em->persist($task);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_show_project', ['id' => $projectId]);
+        }
+
+        return $this->render('tasks/add.html.twig', [
+            'task' => $task,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('task/edit/{taskId}', name: 'app_edit_task', methods: ['GET', 'POST'])]
+    public function editTask(Request $request, int $taskId): Response
+    {
+        $task = $this->em->getRepository(Tasks::class)->find($taskId);
+        if (!$task){
+            throw $this->createNotFoundException('Tâche introuvable');
+        }
+
+        $form = $this->createForm(TaskForm::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task->setUpdatedAt(new DateTimeImmutable());
+
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('tasks/edit.html.twig', [
+            'task' => $task,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('task/delete/{taskId}', name: 'app_delete_task', methods: ['GET','POST'])]
+    public function deleteTask(Request $request, int $taskId): Response
+    {
+        $task = $this->em->getRepository(Tasks::class)->find($taskId);
+
+        if (!$task){
+            throw $this->createNotFoundException('Tâche introuvable');
+        }
+
+            $this->em->remove($task);
+            $this->em->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
+}
---
+++ src/Form/TaskForm.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Projects;
+use App\Entity\Status;
+use App\Entity\Tasks;
+use App\Entity\Users;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre de la tâche',
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'required' => false,
+            ])
+            ->add('deadline', DateType::class, [
+                'label' => 'Date',
+                'widget' => 'single_text',
+                'required' => false,
+            ])
+            ->add('status', EntityType::class, [
+                'class' => Status::class,
+                'choice_label' => 'name',
+                'label' => 'Statut',
+                'required' => true,
+            ])
+            ->add('users', EntityType::class, [
+                'class' => Users::class,
+                'choice_label' => function (Users $users) {
+                    return $users->getFirstName() . ' ' . $users->getLastName();
+                },
+                'label' => 'Membre',
+                'placeholder' => '',
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tasks::class,
+        ]);
+    }
+}
