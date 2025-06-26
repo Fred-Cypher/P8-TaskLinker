fc4c66cdcee17f64f1f12495cda6cb51945c87b9
Fred Perrier <frederique.perrier@gmail.com>
2025-06-26 15:25:26 +0200

    Add part users (list, edit and delete)

---
+++ src/Form/UserForm.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'required' => true,
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'Email',
+                'required' => true,
+            ])
+            ->add('arrivalDate', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date d\'entrée',
+                'required' => true,
+            ])
+            ->add('contract', TextType::class, [
+                'label' => 'Statut',
+                'required' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
